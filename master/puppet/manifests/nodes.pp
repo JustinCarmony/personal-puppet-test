05e41e96237843f2ab107d5b49d8c48e2fcb13ba
Justin Carmony <justin@justincarmony.com>
2012-01-10 22:38:18 -0700

    Adding two other servers

--- master/puppet/manifests/nodes.pp
+++ master/puppet/manifests/nodes.pp
@@ -3,4 +3,13 @@ node basenode {
 }
 
 node 'dev1.justincarmony.com' inherits basenode {
+
+}
+
+node 'dev2.justincarmony.com' inherits basenode {
+
+}
+
+node 'dev3.justincarmony.com' inherits basenode {
+
 }
